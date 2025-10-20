9572221888e61b6fd04bc8dc6ac21827ba0c139c
Bo <44844728+bo-xd@users.noreply.github.com>
2025-10-20 15:50:28 +0200

    Add initial web, server, and config files for agenda app
    Added new PHP and HTML files for login and agenda (Rooster) pages, along with corresponding CSS for styling. Introduced a basic server connection script in server.php. Removed placeholder files hi.html and index.php. Updated .idea configuration files for project and PHP tool integrations.

---
+++ web/Rooster.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/Rooster.css"/>
+    <title>Rooster</title>
+</head>
+<body>
+    <div class="Agenda">
+        <div class="Maandag">
+            <h4 class="DateMaandag"></h4>
+            <ul class="TasksMaandag">
+                <li>test</li>
+            </ul>
+        </div>
+        <div class="Dinsdag">
+            <h4 class="DateDinsdag"></h4>
+            <ul class="TasksDinsdag">
+                <li>test</li>
+            </ul>
+        </div>
+        <div class="Woensdag">
+            <h4 class="DateWoensdag"></h4>
+            <ul class="TasksWoensdag">
+                <li>test</li>
+            </ul>
+        </div>
+        <div class="Donderdag">
+            <h4 class="DateDonderdag"></h4>
+            <ul class="TasksDonderdag">
+                <li>test</li>
+            </ul>
+        </div>
+        <div class="Vrijdag">
+            <h4 class="DateVrijdag"></h4>
+            <ul class="TasksVrijdag">
+                <li>test</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
